c5586c0289d71a81e1c97975d5b7f07cb4de5a24
PawanPawar11 <pawanpawar4369@gmail.com>
2024-08-08 23:20:41 +0530

    third commit

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const { registerUser, loginUser } = require("../controllers/authController");
 
 router.get("/", function (req, res) {
   res.send("Hey from users...");
 });
+
+router.post("/register", registerUser);
+
+router.post("/login", loginUser);
 
 module.exports = router;
