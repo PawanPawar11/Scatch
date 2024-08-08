ff918dd215f51a9c289a80097d4d5f521703e942
PawanPawar11 <pawanpawar4369@gmail.com>
2024-08-09 01:45:36 +0530

    fourth commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const isLoggedIn = require("../middlewares/isLoggedIn");
 
 router.get("/", (req, res) => {
-  const error = [];
+  let error = req.flash("error");
   res.render("index", { error });
+});
+
+router.get("/shop", isLoggedIn, function (req, res) {
+  res.render("shop");
 });
 
 module.exports = router;
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/authController");
+const isLoggedin = require("../middlewares/isLoggedIn");
+const {
+  registerUser,
+  loginUser,
+  logout,
+} = require("../controllers/authController");
 
 router.get("/", function (req, res) {
   res.send("Hey from users...");
@@ -9,5 +14,7 @@ router.get("/", function (req, res) {
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
+
+router.get("/logout", logout);
 
 module.exports = router;
