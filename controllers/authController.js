5c6403928262f67b89bf3aad7af7fdc54a808843
PawanPawar11 <pawanpawar4369@gmail.com>
2024-08-09 23:10:36 +0530

    fifth commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,10 +8,10 @@ module.exports.registerUser = async function (req, res) {
     let { fullName, email, password } = req.body;
 
     let user = await userModel.findOne({ email: email });
-    if (user)
-      return res
-        .status(401)
-        .send("You already have an account, please login...");
+    if (user) {
+      req.flash("error", "You already have an account, please login");
+      return res.redirect("/");
+    }
 
     bcrypt.genSalt(10, function (err, salt) {
       bcrypt.hash(password, salt, async function (err, hash) {
@@ -26,7 +26,8 @@ module.exports.registerUser = async function (req, res) {
           let token = generateToken(user);
           res.cookie("token", token);
 
-          res.send("User created successfully");
+          req.flash("User created successfully");
+          res.redirect("/shop");
         }
       });
     });
@@ -39,15 +40,24 @@ module.exports.loginUser = async function (req, res) {
   let { email, password } = req.body;
 
   let user = await userModel.findOne({ email: email });
-  if (!user) return res.send("Email or Password incorrect");
+  if (!user) {
+    req.flash("error", "Email or Password incorrect");
+    return res.redirect("/");
+  }
 
   bcrypt.compare(password, user.password, function (err, result) {
     if (result) {
       let token = generateToken(user);
       res.cookie("token", token);
-      res.send("You can login");
+      res.redirect("/shop");
     } else {
-      res.send("Email or Password incorrect");
+      req.flash("error", "Email or Password incorrect");
+      return res.redirect("/");
     }
   });
+};
+
+module.exports.logout = function (req, res) {
+  res.cookie("token", "");
+  res.redirect("/");
 };
